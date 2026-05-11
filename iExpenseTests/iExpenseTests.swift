9658cd1a0ed926d8f8ae35fa8cfe8a65ee5ac694
itsshinra <voeurnsovanmakara313@gmail.com>
2026-05-11 16:39:01 +0700

    Initial Commit

---
+++ iExpenseTests/iExpenseTests.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseTests.swift
+//  iExpenseTests
+//
+//  Created by sovanmakara on 11/5/26.
+//
+
+import Testing
+@testable import iExpense
+
+struct iExpenseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
